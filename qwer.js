5597fc22f822096081360d15cc0dde5eb229cf11
beetrt <beetrt@github.com>
2020-11-08 18:18:10 +0500

    sdf

---
+++ qwer.js
@@ -0,0 +1,12 @@
+var favColor = 'red';
+var myFavColors = ['blue', 'red','cyan']
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var denObject = {
+	firstName: 'Den',
+	lastName: 'Ladygin'
+	favColors: ['blue', 'red','cyan'],
+	yearsOld: 16,
+	isMale: true
+	if isMale: true then isFemale: false
+}
